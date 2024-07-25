efe72f1fad525cf6d3e6e9f22aab94cc39e02ba9
ayushdixit210801 <ayushdixit210801@gmail.com>
2024-07-25 15:11:15 +0000

    added basic CRUD functionality to Hotels

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const Hotel = require("./models/hotel");
+const methodOverride = require("method-override");
 
 mongoose.connect("mongodb://localhost:27017/HotelHub");
 
@@ -17,6 +18,10 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride("_method"));
+
 app.get("/", (req, res) => {
 	res.render("home");
 });
@@ -24,10 +29,34 @@ app.get("/hotels", async (req, res) => {
 	const hotels = await Hotel.find({});
 	res.render("hotels/index", { hotels });
 });
-app.get("hotels/:id", async (req, res) => {
+app.get("/hotels/new", (req, res) => {
+	res.render("hotels/new");
+});
+app.post("/hotels", async (req, res) => {
+	const hotel = new Hotel(req.body.hotel);
+	await hotel.save();
+	res.redirect(`/hotels/${hotel._id}`);
+});
+app.get("/hotels/:id", async (req, res) => {
 	const { id } = req.params;
 	const hotel = await Hotel.findById(id);
 	res.render("hotels/show", { hotel });
+});
+app.get("/hotels/:id/edit", async (req, res) => {
+	const { id } = req.params;
+	const hotel = await Hotel.findById(id);
+	res.render("hotels/edit", { hotel });
+});
+app.put("/hotels/:id", async (req, res) => {
+	const hotel = req.body.hotel;
+	const { id } = req.params;
+	await Hotel.findByIdAndUpdate(id, hotel);
+	res.redirect(`/hotels/${id}`);
+});
+app.delete("/hotels/:id", async (req, res) => {
+	const { id } = req.params;
+	await Hotel.findByIdAndDelete(id);
+	res.redirect("/hotels");
 });
 
 app.listen(port, () => {
